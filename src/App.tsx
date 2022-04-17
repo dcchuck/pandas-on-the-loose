196c3955b7354caaad4aa5bbb490715d140264b0
Chuck Danielsson <chuck.danielsson@gmail.com>
2022-04-17 18:36:13 -0400

    pyodide webworker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sample from './Sample.worker';
+const pyodideWorker = new Sample();
+
+const callbacks = {};
+
+pyodideWorker.onmessage = (event) => {
+  const { id, ...data } = event.data;
+  // @ts-ignore
+  const onSuccess = callbacks[id];
+  // @ts-ignore
+  delete callbacks[id];
+  onSuccess(data);
+};
+
+const asyncRun = (() => {
+  let id = 0; // identify a Promise
+  // @ts-ignore
+  return (script, context) => {
+    // the id could be generated more carefully
+    id = (id + 1) % Number.MAX_SAFE_INTEGER;
+    return new Promise((onSuccess) => {
+      // @ts-ignore
+      callbacks[id] = onSuccess;
+      pyodideWorker.postMessage({
+        ...context,
+        python: script,
+        id,
+      });
+    });
+  };
+})();
+
+const script = `
+    import statistics
+    from js import A_rank
+    statistics.stdev(A_rank)
+`;
+
+const context = {
+  A_rank: [0.8, 0.4, 1.2, 3.7, 2.6, 5.8],
+};
+
+async function main() {
+  try {
+    // @ts-ignore
+    const { results, error } = await asyncRun(script, context);
+    if (results) {
+      console.log("pyodideWorker return results: ", results);
+    } else if (error) {
+      console.log("pyodideWorker error: ", error);
+    }
+  } catch (e) {
+    console.log(
+      //@ts-ignore
+      `Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`
+    );
+  }
+}
 
 function App() {
+  React.useEffect(() => { main() }, [])
   return (
     <div className="App">
       <header className="App-header">
--- src/Sample.worker.ts
+++ src/Sample.worker.ts
@@ -7,3 +7,40 @@ console.log('[MyWorker] Running.');
 self.addEventListener('message', (event: MessageEvent): void => {
   console.log('[MyWorker] Incoming message from main thread:', event.data);
 });
+// webworker.js
+
+// Setup your project to serve `py-worker.js`. You should also serve
+// `pyodide.js`, and all its associated `.asm.js`, `.data`, `.json`,
+// and `.wasm` files as well:
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
+
+async function loadPyodideAndPackages() {
+  // @ts-ignore
+  self.pyodide = await loadPyodide();
+  // @ts-ignore
+  await self.pyodide.loadPackage(["numpy", "pytz"]);
+}
+let pyodideReadyPromise = loadPyodideAndPackages();
+
+self.onmessage = async (event) => {
+  // make sure loading is done
+  await pyodideReadyPromise;
+  // Don't bother yet with this line, suppose our API is built in such a way:
+  const { id, python, ...context } = event.data;
+  // The worker copies the context in its own "memory" (an object mapping name to values)
+  for (const key of Object.keys(context)) {
+    // @ts-ignore
+    self[key] = context[key];
+  }
+  // Now is the easy part, the one that is similar to working in the main thread:
+  try {
+    // @ts-ignore
+    await self.pyodide.loadPackagesFromImports(python);
+    // @ts-ignore
+    let results = await self.pyodide.runPythonAsync(python);
+    self.postMessage({ results, id });
+  } catch (error) {
+    // @ts-ignore
+    self.postMessage({ error: error.message, id });
+  }
+};
