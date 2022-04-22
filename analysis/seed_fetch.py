f4fe5382a41478a2eb5e43452e4ef688333e9ffa
Chuck Danielsson <chuck.danielsson@gmail.com>
2022-04-22 14:10:13 -0400

    massive seeding script; TODO extract symbols & point to resources

--- analysis/seed_fetch.py
+++ analysis/seed_fetch.py
@@ -7,6 +7,11 @@ import datetime
 
 start_date = datetime.datetime(2015, 1, 1)
 end_date = datetime.datetime(2022, 1, 1)
+# expetec observation count
+expected = 1763
+
+class NotEnoughObservations(Exception):
+    pass
 
 def get_filename(stock_symbol):
     """
@@ -24,10 +29,10 @@ def create_seed_json_file(stock_symbol):
     stock_data = web.DataReader(stock_symbol, "yahoo", start_date, end_date)
     prices = stock_data.loc[:,"Adj Close"]
     prices = prices.fillna(method="ffill")
+    if len(prices) != expected:
+        raise NotEnoughObservations
 
     prices.to_json(get_filename(stock_symbol))
-
-symbol_list = ["^GSPC", "AAPL", "AMZN", "FB", "GOOG", "MSFT"] #,"CAT", "NKE", "DAL","XOM"
 
 def get_ts_text(stock_symbol):
     """
@@ -37,7 +42,6 @@ def get_ts_text(stock_symbol):
         return f"export const sp500 = "
     else :
         return f"export const {stock_symbol.lower()} = "
-
 
 def add_necessary_typescript_code(stock_symol):
     """
@@ -52,6 +56,39 @@ def add_necessary_typescript_code(stock_symol):
         ts_text = get_ts_text(stock_symol)
         f.write(ts_text + '\n' + content)
 
+# symbol_list = ["^GSPC", "AAPL", "AMZN", "FB", "GOOG", "MSFT"] #,"CAT", "NKE", "DAL","XOM"
+# symbol_list = ["^GSPC", "AAPL", "AMZN", "FB", "GOOG", "MSFT","CAT", "NKE", "DAL","XOM"]
+# redefine symbol list
+index_lines = []
+symbol_list = pd.read_csv("./analysis/nasdaq.csv")["Symbol"]
+
+not_enough_observations_count = 0
+no_data_count = 0
+key_error_count = 0
+# for symbol in nasdaq["Symbol"]:
 for symbol in symbol_list:
-    create_seed_json_file(symbol)
-    add_necessary_typescript_code(symbol)
+    print(f"Fetching {symbol}")
+    try:
+        if symbol == "^GSPC" or symbol.isalpha():
+            create_seed_json_file(symbol)
+            add_necessary_typescript_code(symbol)
+            if symbol == "^GSPC":
+                index_lines.append("export * from './sp500';")
+            else:
+                index_lines.append(f"export * from './{symbol.lower()}';")
+        else:
+            print(f"Skipping: {symbol}")
+    except NotEnoughObservations:
+        not_enough_observations_count += 1
+    except web._utils.RemoteDataError:
+        no_data_count += 1
+    except KeyError:
+        key_error_count += 1
+
+print(f"For {len(symbol_list)} observations...")
+print(f"{not_enough_observations_count} did not have enough observations")
+print(f"{no_data_count} did not have data")
+print(f"{key_error_count} KeyError")
+
+with open("./src/data-seed/index.ts", "w") as outfile:
+    outfile.write("\n".join(index_lines))
