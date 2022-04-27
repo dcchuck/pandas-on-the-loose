dfba064cc93988ebc887f34c6760355e3f4728c0
Chuck Danielsson <chuck.danielsson@gmail.com>
2022-04-26 21:39:18 -0400

    finalize matrix component intial version

---
+++ src/LoadingPanda.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import pandaImage from './panda.png';
+
+// TODO - move necessary
+export const LoadingPanda = () => {
+    return (
+        <img src={pandaImage} className="rotate" alt="panda face"/>
+    )
+}
