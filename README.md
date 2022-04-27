f577b6f08c79315858ddbf92066f34056e8193a8
Chuck Danielsson <chuck.danielsson@gmail.com>
2022-04-26 21:39:02 -0400

    finalize matrix component initial version

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ The typescript will not compile without it
 python3 analysis/seed_fetch.py
 ```
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+<a target="_blank" href="https://icons8.com/icon/9219/panda">Panda</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- src/CorCovMatrix.tsx
+++ src/CorCovMatrix.tsx
@@ -10,36 +10,50 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import { Typography } from "@mui/material";
+import { LoadingPanda } from './LoadingPanda';
 
 const worker = new PyodideWorker();
 
 export enum MatrixDefinition {
-    Correlation = 'Correlation',
-    Covariance = 'Covariance'
+  Correlation = 'Correlation',
+  Covariance = 'Covariance'
 }
 
 const sendRunMessage = async (matrixDefinition: MatrixDefinition, portfolio: string[]) => {
-    worker.postMessage({
-        matrixDefinition,
-        python: correlationMatrixPython,
-        portfolio,
-    })
+  worker.postMessage({
+    matrixDefinition,
+    python: correlationMatrixPython,
+    portfolio,
+  })
 };
 
 interface MatrixTableProps {
-    rows: number[][];
-    columns: string[];
+  rows: number[][];
+  columns: string[];
+}
+
+const LoadingPlaceholder = () => {
+  return (
+    <Paper sx={{ height: 400, width: "100%", display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column', alignItems: 'center' }}>
+      <Box component="div" sx={{ backgroundColor: 'white', display: 'flex', width: 100 }}>
+        <LoadingPanda />
+      </Box>
+      <Typography variant="h6" sx={{ display: 'flex' }}>Loading...</Typography>
+    </Paper>
+  )
 }
 
 const MatrixTable: React.FC<MatrixTableProps> = ({ rows, columns }) => {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+      <Table sx={{ width: '100%' }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             {
-                columns.map(columnString => <TableCell>{columnString}</TableCell>)
+              columns.map((columnString, index) => <TableCell key={`columns.mapTableCell-${index}`}>{columnString}</TableCell>)
             }
           </TableRow>
         </TableHead>
@@ -50,7 +64,7 @@ const MatrixTable: React.FC<MatrixTableProps> = ({ rows, columns }) => {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               {
-                  rowNumbersArray.map(numberValue => <TableCell>{numberValue}</TableCell>)
+                rowNumbersArray.map((numberValue, index) => <TableCell key={`rowNumbersArray.mapTableCell${index}`}>{numberValue}</TableCell>)
               }
             </TableRow>
           ))}
@@ -61,36 +75,64 @@ const MatrixTable: React.FC<MatrixTableProps> = ({ rows, columns }) => {
 }
 
 export const CorCovMatrix: React.FC<{ matrixDefinition: MatrixDefinition }> = ({ matrixDefinition }) => {
-    const [rowData, setRowData] = useState<number[][]>([]);
-    const [columnDefs, setColumnDefs] = useState<string[]>([]);
-    const [portfolio, setPortfolio] = React.useState<string[]>([]);
+  const [rowData, setRowData] = useState<number[][]>([]);
+  const [columnDefs, setColumnDefs] = useState<string[]>([]);
+  const [portfolio, setPortfolio] = React.useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    // TODO this any
-    const onWorkerMessage = useCallback((event: any) => {
-        const { results } = event.data;
-        const [symbolArray, rowsArray]: [string[], number[][]] = JSON.parse(results);
+  // TODO this any
+  const onWorkerMessage = useCallback((event: any) => {
+    const { results } = event.data;
+    const [symbolArray, rowsArray]: [string[], number[][]] = JSON.parse(results);
 
-        setRowData(rowsArray);
-        setColumnDefs(symbolArray)
-    }, [setRowData])
+    setRowData(rowsArray);
+    setColumnDefs(symbolArray)
+    setLoading(false)
+  }, [setRowData])
 
-    useEffect(() => {
-        worker.onmessage = onWorkerMessage;
+  useEffect(() => {
+    worker.onmessage = onWorkerMessage;
 
-        return worker.removeEventListener('message', onWorkerMessage);
-    }, [onWorkerMessage])
+    return worker.removeEventListener('message', onWorkerMessage);
+  }, [onWorkerMessage])
 
-    const submitPortfolio = useCallback(() => {
-        sendRunMessage(matrixDefinition, portfolio)
-    }, [matrixDefinition, portfolio]);
+  const submitPortfolio = useCallback(() => {
+    sendRunMessage(matrixDefinition, portfolio)
+  }, [matrixDefinition, portfolio]);
 
+  useEffect(() => {
+    if (loading) {
+      submitPortfolio()
+    }
+  }, [loading])
 
-    return (
-        <Paper>
-            <Typography variant="h5">{matrixDefinition} Matrix</Typography>
-            <PortfolioSelect portfolio={portfolio} setPortfolio={setPortfolio} />
-            <Button onClick={submitPortfolio}>Submit</Button>
-            <MatrixTable rows={rowData} columns={columnDefs} />
-        </Paper>
-    )
+  const onButtonClick = () => setLoading(true)
+
+  return (
+    <Paper sx={{ height: '100%' }}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item xs={5}>
+          <Typography variant="h5" sx={{ marginLeft: 3 }}>{matrixDefinition} Matrix</Typography>
+        </Grid>
+        <Grid item xs={5}>
+          <PortfolioSelect portfolio={portfolio} setPortfolio={setPortfolio} />
+        </Grid>
+        <Grid item xs={2}>
+          <Button onClick={onButtonClick} size="large" disabled={loading}>Submit</Button>
+        </Grid>
+        <Grid item xs={12}>
+          {
+            loading ?
+              <LoadingPlaceholder />
+              :
+              <MatrixTable rows={rowData} columns={columnDefs} />
+          }
+        </Grid>
+      </Grid>
+    </Paper>
+  )
 }
